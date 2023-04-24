0ecc651ef304a70179ffc20e603d4c71daea1ca8
TravisIPeterson <travisianpeterson@gmail.com>
2023-04-24 16:55:03 -0700

    Complete YAML functionality with load and save.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -2,9 +2,16 @@ require 'yaml'
 
 class GameTools
 
-    attr_accessor :word, :word_array, :guess_array
+    attr_accessor :word, :word_array, :guess_array, :incorrect
 
-    def initialize
+    def initialize(word = "", word_array = "", guess_array = "", incorrect = 0)
+        @word = word
+        @word_array = word_array
+        @guess_array = guess_array
+        @incorrect = incorrect
+    end
+
+    def game_start
         @word = random_word
         @word_array = create_word_array
         @guess_array = create_guess_array
@@ -30,32 +37,70 @@ class GameTools
         return guess_array
     end
 
+    def to_yaml
+        YAML.dump ({
+            :word => @word,
+            :word_array => @word_array,
+            :guess_array => @guess_array,
+            :incorrect => @incorrect
+        })
+    end
+
+    def self.from_yaml(file)
+        data = YAML.load(File.read("saves/#{file}.yml"))
+        p data
+        self.new(data[:word], data[:word_array], data[:guess_array], data[:incorrect])
+    end
+
 end
 
 hangman = GameTools.new
+hangman.game_start
 
-puts "Welcome to Hangman! The computer has randomly selected a word for you (between 5 and 12 letters)."
+puts "Welcome to Hangman! The computer has randomly selected a word for you (between 5 and 12 letters). Also, at any time you can type 'save' or 'load' in order to save this game or load a previous game."
 
-incorrect = 0
 win = false
 
-while incorrect < 6
+while hangman.incorrect < 6
     puts "\n\n#{hangman.guess_array.join()}\n\n
-    You have #{6 - incorrect} guesses left. Which letter would you like to guess?"
+    You have #{6 - hangman.incorrect} guesses left. Which letter would you like to guess?"
     guess = gets.chomp.downcase
-    correct = false
-    hangman.word_array.each_with_index do | letter, index|
-        if letter == guess
-            hangman.guess_array[index] = guess
-            correct = true
+    if guess.downcase == "save"
+        puts "What would you like to name this save?"
+        name = gets.chomp.downcase
+        File.open("saves/#{name}.yml", "w") do |file|
+            file.write(hangman.to_yaml)
         end
-    end
-    if correct == false
-        puts "Sorry, that letter isn't in the word."
-        incorrect += 1
-    elsif hangman.guess_array.join() == hangman.word_array.join()
-        incorrect = 7
-        win = true
+        puts "Your game has been saved."
+        exit
+    elsif guess.downcase == "load"
+        puts "What's the name of your save file?"
+        name = gets.chomp.downcase
+        if File.exist? "saves/#{name}.yml"
+            hangman = GameTools.from_yaml(name)
+            puts "Success! Your game has been loaded."
+        else
+            puts "Sorry, I don't see a save file with that name."
+        end
+    else
+        until guess.length == 1
+            puts "Guess one letter at a time please."
+            guess = gets.chomp
+        end
+        correct = false
+        hangman.word_array.each_with_index do | letter, index|
+            if letter == guess
+                hangman.guess_array[index] = guess
+                correct = true
+            end
+        end
+        if correct == false
+            puts "Sorry, that letter isn't in the word."
+            hangman.incorrect += 1
+        elsif hangman.guess_array.join() == hangman.word_array.join()
+            hangman.incorrect = 7
+            win = true
+        end
     end
 end
 if win == true
